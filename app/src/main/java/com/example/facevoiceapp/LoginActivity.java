9b3ca22abe26bc26c4268b4852788c1315c83e1c
Vedant Sawant <vedant.sawant.2604@gmail.com>
2021-04-18 18:47:24 +0530

    Intents added

--- app/src/main/java/com/example/facevoiceapp/LoginActivity.java
+++ app/src/main/java/com/example/facevoiceapp/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.facevoiceapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.otaliastudios.cameraview.CameraListener;
@@ -39,12 +41,22 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // do something
                 // Select output file. Make sure you have write permissions.
-                File file = new File(getExternalFilesDir(null),"video_file_name");
+                File file = new File(getExternalFilesDir(null),"video_file_name.mp4");
                 cameraView.setMode(Mode.VIDEO);
                 cameraView.takeVideoSnapshot(file,5000000);
+                Log.i("Camera","Video Captured"+ file);
+                try {
+                    wait(5000000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
                 // Later... stop recording. This will trigger onVideoTaken().
                 //cameraView.stopVideo();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                //intent.putExtra("USER_ID", sessionId);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/facevoiceapp/MainActivity.java
+++ app/src/main/java/com/example/facevoiceapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.facevoiceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button reg_btn,log_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reg_btn = findViewById(R.id.reg_btn);
+        log_btn = findViewById(R.id.log_btn);
+
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+                //intent.putExtra("USER_ID", sessionId);
+                startActivity(intent);
+            }
+        });
+
+        log_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                //intent.putExtra("USER_ID", sessionId);
+                startActivity(intent);
+            }
+        });
     }
 }
